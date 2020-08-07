a4b5a9c05b21830b660c854c5e2d9852acdf518f
Oscar Diaz <oads7@outlook.com>
2020-08-07 17:29:16 -0500

    Add function add (), two ints

--- Hello.cc
+++ Hello.cc
@@ -1,7 +1,13 @@
 #include <iostream>
 
+int add (const int& a, const int& b) {
+	return a+b;
+}
+
 int main () {
 	std::cout << "Hello, World\n";
+	std::cout << "Sumar 1+2 = " << add (1,2) << "\n";
+	
 	return 0;
 }
 
